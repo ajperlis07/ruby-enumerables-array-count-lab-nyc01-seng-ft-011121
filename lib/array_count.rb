819cc0a0da9a971f4d032b21518095722949a7e5
Andrew  Perlis <ajperlis07@gmail.com>
2021-01-07 01:23:13 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,5 +1,5 @@
 def count_strings(array)
-  count_strings = [1,2,3,4].count 
+  count_strings = [array].length 
   count_strings
   # Return the total number of strings in the provided array using the count enumerable
 end
